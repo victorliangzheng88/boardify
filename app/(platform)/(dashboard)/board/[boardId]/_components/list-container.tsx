6facff3f8b01fa0f4766f9d8108c9ea74db965e9
Victor Liang Zheng <60474948+victorliangzheng88@users.noreply.github.com>
2024-04-03 21:09:27 -0400

    modify single list component

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -3,18 +3,25 @@
 import { ListWithCards } from "@/types";
 
 import { ListForm } from "./list-form";
+import { useEffect, useState } from "react";
+import { ListItem } from "./list-item";
 
 interface ListContainerProps {
   data: ListWithCards[];
   boardId: string;
-};
+}
 
-export const ListContainer = ({
-  data,
-  boardId,
-}: ListContainerProps) => {
+export const ListContainer = ({ data, boardId }: ListContainerProps) => {
+  const [orderedData, setOrderedData] = useState(data);
+
+  useEffect(() => {
+    setOrderedData(data);
+  }, [data]);
   return (
-    <ol>
+    <ol className="flex gap-x-3 h-full">
+      {orderedData.map((list, index) => {
+        return <ListItem key={list.id} index={index} data={list} />;
+      })}
       <ListForm />
       <div className="flex-shrink-0 w-1" />
     </ol>
